79f603f3c93291c8a553f08eb23b49f02d723171
mabroor007 <amabroor007@gmail.com>
2021-06-16 03:13:09 +0500

    rust docs are amazing

---
+++ src/docs/mod.rs
@@ -0,0 +1,16 @@
+// Rust docs are amazing. No setup required and battries included
+
+// let's say sum is a function i want to document
+// all i have to do is to put /// on top of that
+// function and start writing documentation.
+// Bonus! It also supports markdown as well
+
+/// adds two numbers
+///
+/// # Arguments
+///
+/// * `a` - first number
+/// * `b` - second number
+pub fn sum(a: i32, b: i32) -> i32 {
+    a + b
+}
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+mod docs;
+
+fn main() {
+    let result = docs::sum(6, 9);
+    println!("{}", result)
+}
