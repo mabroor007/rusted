0da6bb82ab5a467fd575419658b25e7c99001c49
mabroor007 <amabroor007@gmail.com>
2021-06-25 15:46:29 +0500

    bitwise operation and shifting

--- src/arithmatics/mod.rs
+++ src/arithmatics/mod.rs
@@ -13,4 +13,13 @@ pub fn sample() {
     println!("5 in binary is {:b}", 5);
     println!("25 in hex is {:x}", 15);
     println!("0xfff in decimal is {}", 0xf);
+
+    // Bitwise operations
+    println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
+    println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
+    println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
+    // shift bits to right
+    println!("00000001 << 5 is {:b}", 1u8 << 6);
+    // shift bits to left
+    println!("00000001 >> 1 is {:b}", 1u8 >> 2);
 }
