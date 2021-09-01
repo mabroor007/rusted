0814a84daf3a07c2cf26c57538695ade7ceebb6c
mabroor007 <amabroor007@gmail.com>
2021-09-01 21:49:37 +0500

    added details

--- src/ownership/mod.rs
+++ src/ownership/mod.rs
@@ -2,19 +2,6 @@ pub fn gives_ownership_of_string() -> String {
     String::from("ninja")
 }
 
-pub fn run() {
-    let mut some_string = gives_ownership_of_string();
-    some_string.push_str(" is a cool guy.");
-
-    // redeclare with same name results in shadowing
-    let some_string = takes_ownership_of_string_then_gives_back(some_string);
-
-    takes_ownership_of_string(some_string);
-
-    // throws error because ownership was given away
-    // some_string.push_str(" and he is an engineer");
-}
-
 pub fn takes_ownership_of_string(s: String) {
     println!("i took the ownership of '{}' and i won't give it back", s);
 }
@@ -25,4 +12,19 @@ pub fn takes_ownership_of_string_then_gives_back(s: String) -> String {
         s
     );
     s
+}
+
+pub fn run() {
+    let mut some_string = gives_ownership_of_string();
+
+    // this scope owns some_string so it can modify the variable
+    some_string.push_str(" is a cool guy.");
+
+    // redeclare with same name results in shadowing
+    let some_string = takes_ownership_of_string_then_gives_back(some_string);
+
+    takes_ownership_of_string(some_string);
+
+    // throws error because ownership was given away and some_string is not in scope
+    // some_string.push_str(" and he is an engineer");
 }
